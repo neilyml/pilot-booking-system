5b162eb778a33f23515f6c224ad77efbf00578c3
neilyml <55622147+neilyml@users.noreply.github.com.>
2026-08-12 21:19:13 +0600

    initialize repository base

---
+++ src/main/java/com/aiimglobal/pilot/booking/system/PilotBookingApplication.java
@@ -0,0 +1,13 @@
+package com.aiimglobal.pilot.booking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilotBookingApplication.class, args);
+    }
+
+}
